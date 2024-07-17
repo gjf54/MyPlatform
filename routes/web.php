321e3054618c6968eb910ce3f2f58e44d120d001
gjf54 <trrt1004@gmail.com>
2024-07-17 23:14:51 +0300

    1.0.3.2 changes for server

--- routes/web.php
+++ routes/web.php
@@ -193,3 +193,6 @@ Route::group(['middleware' => ['auth', 'role:admin|manager|super_admin|writer'],
     });
 });
 
+
+Route::get('/linkstorage', function () { $targetFolder = base_path().'/storage/app/public'; $linkFolder = $_SERVER['DOCUMENT_ROOT'].'/storage'; symlink($targetFolder, $linkFolder); });
+
